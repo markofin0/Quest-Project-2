9b1b7ee416b4dee10ed6497c82a4d2cd50d8085f
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-04-14 23:25:39 -0400

    Made a new main scene, and put everything together

--- UnityQuest2020BalloonTemplate/Assets/Scripts/RockStack.cs
+++ UnityQuest2020BalloonTemplate/Assets/Scripts/RockStack.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
-using static UnityEditor.FilePathAttribute;
 
 public class RockStack : MonoBehaviour
 {
