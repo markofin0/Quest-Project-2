08d5fab727b7206247d8ebbae8447846c0a3e26e
markofin0 <m.marullo25@ncf.edu>
2024-04-18 10:04:41 -0400

    Lantern + End scene + Day/Night

---
+++ UnityQuest2020BalloonTemplate/Assets/Scripts/LanternFloat.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternFloat : MonoBehaviour
+{
+    private OVRGrabbable m_GrabScript;
+    public bool hasBeenGrabbed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_GrabScript = GetComponent<OVRGrabbable>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_GrabScript.isGrabbed)
+        {
+            hasBeenGrabbed = true;
+            
+        }
+        if(!(m_GrabScript.isGrabbed))
+        {
+            if(hasBeenGrabbed == true)
+            {
+                transform.position += Vector3.up * Time.deltaTime;
+            }
+        }
+    }
+}
